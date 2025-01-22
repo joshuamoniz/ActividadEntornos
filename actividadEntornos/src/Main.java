926459fe71f0477b249dcc35deb968259c7565a3
joshuamoniz <joshuaalejandromonizurgiles@a.cifpperedesongall.org>
2025-01-22 16:37:41 +0100

    Funcion de añadir un producto al inventario

--- actividadEntornos/src/Main.java
+++ actividadEntornos/src/Main.java
@@ -1,15 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static String[] anadirProducto(String palabra, String[] inventarios) {
+        String[] newInventario = Arrays.copyOf(inventarios, inventarios.length + 1);
+        newInventario[newInventario.length -1] = palabra;
+        return newInventario;
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        int continuar = 0;
+        String[] inventario = {"Ordenador", "Mesa", "Silla", "Carrito", "Teclado", "Cargador",};
+
+            Scanner sc = new Scanner(System.in);
+            String palabranueva = sc.nextLine();
+            inventario=anadirProducto(palabranueva,inventario);
+            System.out.println(Arrays.toString(inventario));
+
     }
 }
